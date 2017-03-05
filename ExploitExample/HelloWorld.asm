45a135fdb3063cfc3cd376c560287e66b36a9c8f
ACandeias <AnthonyCandeias@gmail.com>
2017-03-05 14:14:23 -0500

    Add files via upload

---
+++ ExploitExample/HelloWorld.asm
@@ -0,0 +1,24 @@
+section .text
+
+    global _start
+
+_start:
+
+        ; write our string to stdout.
+
+        mov     edx,len   ; third argument: message length.
+        mov     ecx,msg   ; second argument: pointer to message to write.
+        mov     ebx,1     ; first argument: file handle (stdout).
+        mov     eax,4     ; system call number (sys_write).
+        int     0x80	  ; call kernel.
+
+        ; and exit.
+
+	mov	ebx,0	  ; first syscall argument: exit code.
+        mov     eax,1     ; system call number (sys_exit).
+        int     0x80	  ; call kernel.
+
+section .data
+
+msg     db      "Hello, world!",0xa      ; the string to print.
+len     equ     $ - msg                  ; length of the string.
